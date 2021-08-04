d9de5599b9d4903f136459e1db134e9905fcd6cd
Paz Ortega <paz.ortega.contreras@gmail.com>
2021-08-04 14:18:10 +0000

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -29,17 +29,17 @@
 
 //ex3
 
-const temperature = 16;
+// const temperature = 16;
 
-if (temperature < 0) {
-  console.log("Make sure you pick out a scarf!");
-} else if (temperature < 15) {
-  console.log("Short sleeves won't cut it!");
-} else {
-  console.log("Short sleeves are fine.");
-}
+// if (temperature < 0) {
+//   console.log("Make sure you pick out a scarf!");
+// } else if (temperature < 15) {
+//   console.log("Short sleeves won't cut it!");
+// } else {
+//   console.log("Short sleeves are fine.");
+// }
 
-console.log("Now you're ready to go outside!");
+// console.log("Now you're ready to go outside!");
 
 //explained
 //This looks a lot more complicated, so let's go through it step by step. 
@@ -52,4 +52,46 @@ console.log("Now you're ready to go outside!");
 // temperature was neither less than 0 nor less than 15, then the else
 // block would be executed.
 
+//Logical operators
 
+// if statements become even more powerful when we understand how to use
+// logical operators in conjunction with them. JavaScript's logical operators
+// are:
+
+// && - logical AND
+// || - logical OR
+// ! - logical NOT.
+// We can use these to write more complex expressions. For example, if a 
+//variable isCitizen holds a Boolean value representing whether or not
+// someone is a citizen, and age is that same person's age, we could write 
+//the following if statement.
+
+// const isCitizen = true;
+// const age = 26;
+
+// if (isCitizen && age > 18) {
+//   console.log("You are eligible to vote.");
+// }
+//
+// Here we're checking that both conditions, isCitizen and age > 18 are true
+// for the if statement overall to evaluate to true. If any of our two 
+//conditions are false, then the whole expression is false.
+
+// Let's consider another example.
+
+// const temperature = 45
+// if (temperature < -40 || temperature > 40) {
+//     console.log("Maybe going outside isn't such a great idea…");
+//   }
+
+//Let's take a look at one more example.
+
+const raining = false
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
+
+//We read the expression in the above example usually as "if not raining",
+// but it's not always intuitive what "not true" or "not false" mean unless
+// you remember that the ! operator reverses the value of raining. That is,
+// if raining is true, !raining is false. If raining is false, !raining is true.
