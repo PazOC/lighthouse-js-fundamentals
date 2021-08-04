abf66db8a38cd1fe8cb076ae7ca70588a394d422
Paz Ortega <paz.ortega.contreras@gmail.com>
2021-08-04 21:19:15 +0000

    ¨modified choru-while file and conditionals file

--- chorus-while.js
+++ chorus-while.js
@@ -1,3 +1,4 @@
+/*We've been working with a pop idol on a new song, and to make sure it's a bonafide ear worm, the label wants us to repeat the chorus ten times at the end of the song. Wanting to practice some JavaScript, we decide to write a while loop to print out the chorus ten times.*/
 const chorus = "Let's dance!";
 let repeat = 0;
 while (repeat < 10) {
@@ -5,3 +6,34 @@ while (repeat < 10) {
   repeat++;
 }
 console.log("Until the sun comes up!");
+/*We should see "Let's dance!" printed 10 times, followed by "Until the sun comes up!".
+
+How does this work? The while statement, similar to an if, takes a condition, and as long as this condition evaluates to true, the code inside the block will be executed again and again. To make sure we don't execute this code forever, we've declared a repeat variable that starts at 0 and increments by 1 (repeat++ is shorthand for repeat = repeat + 1) at every repetition of the block. The following is a representation of what our condition looks like at every step.*/
+(0 < 10) // repeat is 0
+(1 < 10) // repeat is 1
+(2 < 10) // repeat is 2
+(3 < 10) // repeat is 3
+(4 < 10) // repeat is 4
+(5 < 10) // repeat is 5
+(6 < 10) // repeat is 6
+(7 < 10) // repeat is 7
+(8 < 10) // repeat is 8
+(9 < 10) // repeat is 9
+(10 < 10) // repeat is 10
+
+/*When the value of repeat reaches 10, our while condition is no longer true and so JavaScript stops executing the contents of the block following the while statement and moves on.
+
+Like blocks following if statements, any code can go inside the block following a while. For example, if we're worried our song outro is too repetitive we might decide to change the key after five repetitions.*/
+const chorus = "Let's dance!";
+let repeat = 0;
+while (repeat < 10) {
+  if (repeat === 5) {
+    console.log("*change key*");
+  }
+  console.log(chorus);
+  repeat++;
+}
+console.log("Until the sun comes up!");
+/*Here, before printing the chorus for the sixth time (sixth because we've started counting at 0, not 1), we also instruct our pop star to change key before continuing. This works because the value of repeat will be equal to 5 only once—on the sixth iteration of our loop.*/
+
+
--- conditionals.js
+++ conditionals.js
@@ -1,114 +1,78 @@
-// const whichSchool  = function (age) {
-//   if (age < 13){
-//     return "Elementary School";
-//   } else if (age >= 13 && age <= 18) {
-//     return "Secondary School";
-//   } else {
-//     return "Lighthouse Labs";
-//   }
-// };
+/*In our condition, raining is true, and therefore the console log will remind us to pack an umbrella. If raining were false, the code would do nothing.*/
+const raining = true;
 
-// var x = 1;
-// while (x <= 10000) {
-//   console.log(x + "mississippi!");
-//   x = x + 1;
-// }
-
-// var x = 1;
-
-// // while loop with a stop condition
-// while (x <= 20) {
-//     if (x % 3 === 0 && x % 5 === 0) {
-//         console.log("JuliaJames");
-//     }else if (x % 5 === 0) {
-//         console.log("James");
-//     }else if (x % 3 === 0) {
-//         console.log("Julia");
-//     }
-//     else {
-//         console.log(x);
-//     }
-//     // increment x at the end of each loop
-//     x = x + 1 ; 
-// }
-
-// var num = 99;
-
-// while (num >=1) {
-//     // Last iteration. Note occurances of bottle, bottle, bottleS
-//     if (num === 1) {
-//         console.log(num + " bottle of juice on the wall! "
-//                     + num + " bottle of juice! Take one down, pass it around... "
-//                     + (num-1) + " bottles of juice on the wall!");
-//     }
-//     // Second-last iteration. Note occurances of bottleS, bottleS, bottle
-//     else if (num === 2){
-//         console.log(num + " bottles of juice on the wall! "
-//                     + num + " bottles of juice! Take one down, pass it around... "
-//                     + (num-1) + " bottle of juice on the wall!");
-//     }
-//     // All other iterations. Note occurances of bottleS, bottleS, bottleS
-//     else {
-//         console.log(num + " bottles of juice on the wall! "
-//                     + num + " bottles of juice! Take one down, pass it around... "
-//                     + (num-1) + " bottles of juice on the wall!");
-//     }
-//     num = num - 1;
-// }
-
-// n = 60
-
-// // While loop with a stop condition
-// while (n >= 0) {
-//     if (n === 50) {
-//         console.log("Orbiter transfers from ground to internal power");
-//     }
-//     else if (n === 31) {
-//         console.log("Ground launch sequencer is go for auto sequence start");
-//     }
-//     else if (n === 16) {
-//         console.log("Activate launch pad sound suppression system");
-//     }
-//     else if (n === 10) {
-//         console.log("Activate main engine hydrogen burnoff system");
-//     }
-//     else if (n === 6) {
-//         console.log("Main engine start");
-//     }
-//     else if (n === 0) {
-//         console.log("Solid rocket booster ignition and liftoff!");
-//     }
-//     else{
-//         console.log("T-"+n+" seconds");
-//     }
-
-//     //Never forget to decrement/increment the iteration variable in a while loop
-//     // Otherwise, you loop will run infinite iterations
-//     n = n-1;
-// }
-
-// for (var i = 0; i < 6; i = i + 1) {
-//   console.log("Printing out i = " + i);
-// }
-
-//nested loop
-for (var x = 0; x < 5; x = x + 1) {
-  for (var y = 0; y < 3; y = y + 1) {
-    console.log(x + "," + y);
-  }
-} 
-
-//seat 
-var row = 0;  // initial value of the row
-var seat = 0; // initial value of the seat within a row
-
-// One loop inside another is called Nested loop.
-// Outer `for` loop, to iterate over the rows
-for (row = 0; row <= 25; row++){
-
-    // Inner `for` loop, to iterate over the seats within a row
-    // In this loop, the value of `row` variable would change only after 100 iterations
-    for(seat = 0; seat <= 99; seat++){
-        console.log(row+"-"+seat);
-    }
+if (raining) {
+  console.log("Don't forget your umbrella!");
 }
+//Because it is raining but it is not cold, we'll be reminded to grab an umbrella before we're ready to go outside.
+
+const raining = true;
+const cold = false;
+
+if (raining) {
+  console.log("Don't forget your umbrella!");
+}
+
+if (cold) {
+  console.log("Make sure you pick out a scarf!");
+}
+
+console.log("Now you're ready to go outside!");
+//But what if we wanted to do one thing in one case and another in all other cases?
+
+const cold = false;
+
+if (cold) {
+  console.log("Make sure you pick out a scarf!");
+} else {
+  console.log("Short sleeves are fine.");
+}
+/*Now, because cold is false, the console.log within our if statement will not be executed, but the code inside the else block will. If cold were true, the opposite would happen—that is, the program would tell us to choose a scarf to wear but it won't tell us short sleeves are OK.
+
+Doesn't that seem like a bit of a jump, going from short sleeves straight to a scarf? Let's see if we can do better.*/
+const temperature = 12;
+
+if (temperature < 0) {
+  console.log("Make sure you pick out a scarf!");
+} else if (temperature < 15) {
+  console.log("Short sleeves won't cut it!");
+} else {
+  console.log("Short sleeves are fine.");
+}
+
+console.log("Now you're ready to go outside!");
+/*We can use these to write more complex expressions. For example, if a variable isCitizen holds a Boolean value representing whether or not someone is a citizen, and age is that same person's age, we could write the following if statement.*/
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+}
+/*Here we're checking that both conditions, isCitizen and age > 18 are true for the if statement overall to evaluate to true. If any of our two conditions are false, then the whole expression is false.
+
+Let's consider another example.*/
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea…");
+}
+
+/*Here our if statement is true when temperature is either below -40 or above 40 degrees.
+
+Let's take a look at one more example.*/
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
+/*We read the expression in the above example usually as "if not raining", but it's not always intuitive what "not true" or "not false" mean unless you remember that the ! operator reverses the value of raining. That is, if raining is true, !raining is false. If raining is false, !raining is true.*/
+
+
+
+
+
+const whichSchool  = function (age) {
+  if (age < 13){
+    return "Elementary School";
+  } else if (age >= 13 && age <= 18) {
+    return "Secondary School";
+  } else {
+    return "Lighthouse Labs";
+  }
+};
