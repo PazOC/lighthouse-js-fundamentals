c454fa58083bb7ca130afd042a6896176c31098d
Paz Ortega <paz.ortega.contreras@gmail.com>
2021-08-05 20:14:28 +0000

    added plenty of devs challenge

---
+++ PlentyOfDevs.js
@@ -0,0 +1,6 @@
+function ageCalculator(name, yearOfBirth, currentYear) {
+  return name + " is " + (currentYear - yearOfBirth) + " years old.";
+}
+
+console.log(ageCalculator("Pamelita", 1983, 2015));
+console.log(ageCalculator("Leo", 1988, 2015));
