79b36eff9d9c7d1d3809a7082d9692d6d3130e80
Paz Ortega <paz.ortega.contreras@gmail.com>
2021-08-05 19:49:03 +0000

    added finalPositions file

--- chooseStations.js
+++ chooseStations.js
@@ -12,12 +12,6 @@ Your function will receive an array of stations, where each station itself is an
 
 In order for a station to be deemed appropriate, it must have a capacity of at least 20, and be a school or community centre.
 */
-// const stations = [
-//   ['Big Bear Donair', 10, 'restaurant'],
-//   ['Bright Lights Elementary', 50, 'school'],
-//   ['Moose Mountain Community Centre', 45, 'community centre']
-// ];
-
 const stations = [
   ['Big Bear Donair', 10, 'restaurant'],
   ['Bright Lights Elementary', 50, 'school'],
