bd46ab9331d2aa105ba8e34efb5ddb5fb7169dcb
Paz Ortega <paz.ortega.contreras@gmail.com>
2021-08-06 15:36:33 +0000

    created packing-list file

--- ArrayOperationsAndProperties.js
+++ ArrayOperationsAndProperties.js
@@ -1,28 +1,28 @@
-// const director = "Steven Spielberg";
-// const film1 = "Jaws";
-// const film2 = "Raiders of the Lost Ark";
-// const film3 = "A.I.";
+// // const director = "Steven Spielberg";
+// // const film1 = "Jaws";
+// // const film2 = "Raiders of the Lost Ark";
+// // const film3 = "A.I.";
+
+// // const director = "Steven Spielberg";
+// // const films = ["Jaws", "Raiders of the Lost Ark", "A.I."];
+
+// // console.log("My favourite " + director + " film is " + films[1]);
+
+// // "films at index 0: " + films[0];
+// // "films at index 1: " + films[1];
+// // "films at index 2: " + films[2];
+// // "films at index 3: " + films[3];
+
+// // const oddNumbers = [1, 3, 5, 7, 9];
+// // const randomStuff = [1.15, "bicycle", true];
 
 // const director = "Steven Spielberg";
 // const films = ["Jaws", "Raiders of the Lost Ark", "A.I."];
 
-// console.log("My favourite " + director + " film is " + films[1]);
+// films[2] = "Jurassic Park";
+// films;
+// films.push("A.I.");
+// films;
+// console.log(films);
 
-// "films at index 0: " + films[0];
-// "films at index 1: " + films[1];
-// "films at index 2: " + films[2];
-// "films at index 3: " + films[3];
-
-// const oddNumbers = [1, 3, 5, 7, 9];
-// const randomStuff = [1.15, "bicycle", true];
-
-const director = "Steven Spielberg";
-const films = ["Jaws", "Raiders of the Lost Ark", "A.I."];
-
-films[2] = "Jurassic Park";
-films;
-films.push("A.I.");
-films;
-console.log(films);
-
-director + " is the director of " + films.length + " films";
+// director + " is the director of " + films.length + " films";
