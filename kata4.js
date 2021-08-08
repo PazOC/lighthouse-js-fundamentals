e6d3ac117acae3a1a582250a586bb62b4761ed66
Paz Ortega <paz.ortega.contreras@gmail.com>
2021-08-08 07:56:26 +0000

    In this exercise, we will be given a list of instructors and have to determine which instructor has the longest name

---
+++ kata4.js
@@ -0,0 +1,30 @@
+const instructorWithLongestName = function (instructors) {
+  let longest = "";
+  let index = 0;
+  let object;
+  for (let i = 0; i < instructors.length; i++) {
+    let nameLength = instructors[i].name;
+    if (longest.length < nameLength.length) {
+      longest = nameLength;
+      index = longest.indexOf(longest[i]);
+      object = instructors[index];
+    }
+  }
+  return object;
+};
+
+console.log(
+  instructorWithLongestName([
+    { name: "Samuel", course: "iOS" },
+    { name: "Jeremiah", course: "Web" },
+    { name: "Ophilia", course: "Web" },
+    { name: "Donald", course: "Web" },
+  ])
+);
+console.log(
+  instructorWithLongestName([
+    { name: "Matthew", course: "Web" },
+    { name: "David", course: "iOS" },
+    { name: "Domascus", course: "Web" },
+  ])
+);
